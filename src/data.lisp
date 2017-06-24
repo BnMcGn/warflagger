5a7bdfd07d0aa3d05f56ea287685bb2cc9395a48
bnmcgn@gmail.com <bnmcgn@gmail.com>
2017-06-24 07:12:18 -0700

    Removed :insightful :informative. People can indicate those shades of meaning under a :like flag. Removed :point-taken. Not distinct enough in meaning. May reverse that one. Added :interesting. Should be more useful than above flags.

--- src/data.lisp
+++ src/data.lisp
@@ -22,8 +22,7 @@ especially if you feel that its point could have been made more gently."
      "For off-color or coarse language."
 
      :disturbing
-     "Warning that a link is pornographic, disturbing, or otherwise not work
-safe."
+     "Warning that a link is pornographic, disturbing, or otherwise not safe for work."
 
      :already-answered
      "If you feel that a point has been answered well in another place, use this flag's reference field to point to the existing answer, rather than restating it
@@ -62,24 +61,18 @@ exploration.")
 
 ;;; Positive:
 
-    (:informative
-     " Informative comments add new information to explain the circumstances hinted at by a particular story, fill in \"The Other Side\" of an argument, etc."
-
-     :insightful
-     "An Insightful comment makes you think, or puts a new spin on a given story. Examples: an analogy you hadn't thought of, or a telling counterexample."
-
-     :funny
+    (:funny
      "For the evident use. Note: Some people don't feel that the upvoting of Funny
  comments adds to a discussion, therefore they may regard a positive Funny vote as a proxy for Offtopic."
-
-     :point-taken
-     "A way of acknowledging that an opponent has resolved something in his favor."
 
      :agree
      "Generic agreement: When you think that something is right."
 
      :like
-     "Even more generic approval: Use this for things that are a matter of taste more than a matter of fact or opinion.")
+     "Even more generic approval: Use this for things that are a matter of taste more than a matter of fact or opinion."
+
+     :interesting
+     "Use this when you wish to indicate that something is worth noting, even if you aren't ready to agree with it. Interesting flags used with a negative vote value will still raise the profile of a target. It can be used to point out negative examples.")
 
 ;;; SpecialClaims
 
@@ -109,7 +102,7 @@ therefore left as an option for the individual.")
 field."
 
      :out-of-date
-     "Some comments are going to need pruning as time progresses."
+     "Some comments are going to need pruning as time progresses. When appropriate, add a link to updated statements using the Reference field."
 
      :retraction
      "This flag is only valid for use against your own posts. Use it to acknowledge
@@ -155,7 +148,7 @@ throw the discussion tree out of whack."
   '(("Spam" "Inflammatory" "Disagree" "Dislike" "Obscene" "Disturbing"
      "AlreadyAnswered" "LogicalFallacy" "AdHominem" "FromAuthority"
      "NeedsReference" "RaiseQuestion")
-    ("Informative" "Insightful" "Funny" "PointTaken" "Agree" "Like")
+    ("Funny" "Agree" "Like" "Interesting")
     ("EyeWitness" "AmQualified" "SecondHand" "Disclosure")
     ("Redundant" "OutOfDate" "Retraction" "Correction" "Update" "IncorrectFlag"
      "FlagAbuse" "Offtopic" "Arcane" "SameThing")))
@@ -165,6 +158,7 @@ throw the discussion tree out of whack."
 ;;; [True, True, False, 1] allows neg and neutral values with neutral as the
 ;;; default
 
+;;FIXME: out of date
 (defparameter *vote-ranges*
   '((t nil nil 0) (t nil nil 0) (t nil nil 0) (t nil nil 0)
     (t nil nil 0) (t nil nil 0) (t t nil 0) (t nil nil 0)
