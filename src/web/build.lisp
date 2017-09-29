9f152a16c0cb2817bd1583886ba99b551b488e24
bnmcgn@gmail.com <bnmcgn@gmail.com>
2017-09-29 11:29:10 -0700

    Add swank server to lisp image

--- src/web/build.lisp
+++ src/web/build.lisp
@@ -3,8 +3,11 @@
 ;;;
 
 (ql:quickload 'wf-web :silent nil)
+(ql:quickload 'swank)
 
 (defun main ()
+  (setf swank:*use-dedicated-output-stream* nil)
+  (swank:create-server :port 4004 :dont-close t)
   (wf/web::run-server))
 
 (sb-ext:save-lisp-and-die
