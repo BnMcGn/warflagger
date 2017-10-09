3d2df3f7cb249d2c53c4707aedf069e139d38f83
bnmcgn@gmail.com <bnmcgn@gmail.com>
2017-10-09 07:13:15 -0700

    Attempt to fix image compilation problem

--- src/web/build.lisp
+++ src/web/build.lisp
@@ -2,11 +2,15 @@
 ;;; Script to build a lisp server image for warflagger.net
 ;;;
 
-(ql:quickload 'wf-web :silent nil)
+(ql:quickload 'wf-web :silent nil
 (ql:quickload 'swank)
 
 (defun main ()
   ;;(setf swank:*use-dedicated-output-stream* nil)
+  (setf asdf:*user-cache*
+        (gadgets:extend-pathname (asdf/cl:user-homedir-pathname)
+                                 ".cache" "common-lisp"
+                                 (asdf:implementation-identifier)))
   (swank:create-server :port 4004 :dont-close t)
   (wf/web::run-server))
 
