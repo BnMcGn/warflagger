f79298f0ee715521e38654621d3345d5fa7cdcf4
bnmcgn@gmail.com <bnmcgn@gmail.com>
2017-10-11 08:33:48 -0700

    Try using require instead of quickload

--- src/web/build.lisp
+++ src/web/build.lisp
@@ -2,18 +2,20 @@
 ;;; Script to build a lisp server image for warflagger.net
 ;;;
 
-(pushnew (truename "/home/ben/quicklisp/local-projects/")
-         ql:*local-project-directories* )
+;(pushnew (truename "/home/ben/quicklisp/local-projects/")
+;         ql:*local-project-directories*)
 
-(ql:quickload 'wf-web :silent nil)
-(ql:quickload 'swank)
+;(ql:quickload 'wf-web :silent nil)
+;(ql:quickload 'swank)
+(require 'wf-web)
+(require 'swank)
 
 (defun main ()
   ;;(setf swank:*use-dedicated-output-stream* nil)
-  (setf asdf:*user-cache*
-        (gadgets:extend-pathname (asdf/cl:user-homedir-pathname)
-                                 ".cache" "common-lisp"
-                                 (asdf:implementation-identifier)))
+;  (setf asdf:*user-cache*
+;        (gadgets:extend-pathname (asdf/cl:user-homedir-pathname)
+;                                 ".cache" "common-lisp"
+;                                 (asdf:implementation-identifier)))
   (swank:create-server :port 4004 :dont-close t)
   (wf/web::run-server))
 
