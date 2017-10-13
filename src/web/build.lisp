72e2e122274bcfec59a51f50d4401c1346e0be92
bnmcgn@gmail.com <bnmcgn@gmail.com>
2017-10-13 07:01:17 -0700

    Final touchup to build.lisp. Cleanup

--- src/web/build.lisp
+++ src/web/build.lisp
@@ -2,20 +2,12 @@
 ;;; Script to build a lisp server image for warflagger.net
 ;;;
 
-;(pushnew (truename "/home/ben/quicklisp/local-projects/")
-;         ql:*local-project-directories*)
-
-;(ql:quickload 'wf-web :silent nil)
-;(ql:quickload 'swank)
-(require 'wf-web)
-(require 'swank)
+(ql:register-local-projects)
+(ql:quickload 'wf-web :silent nil)
+(ql:quickload 'swank)
+;;(ql:quickload 'trivial-features)
 
 (defun main ()
-  ;;(setf swank:*use-dedicated-output-stream* nil)
-;  (setf asdf:*user-cache*
-;        (gadgets:extend-pathname (asdf/cl:user-homedir-pathname)
-;                                 ".cache" "common-lisp"
-;                                 (asdf:implementation-identifier)))
   (swank:create-server :port 4004 :dont-close t)
   (wf/web::run-server))
 
--- src/web/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mv wf-server.old wf-server.older
-mv wf-server.img wf-server.old
-
-sbcl --load build.lisp
