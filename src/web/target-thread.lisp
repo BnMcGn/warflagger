34970ff7cb989eb730d9a02273f80d7956e83dff
bnmcgn@gmail.com <bnmcgn@gmail.com>
2018-01-30 06:27:52 -0800

    Feed target text into thread-opinion

--- src/web/target-thread.lisp
+++ src/web/target-thread.lisp
@@ -12,25 +12,40 @@
           (:whatever))))
 
     (def-component thread-opinion
-        (psx
-         (:div
-          (:vote-value :key 1 :opinion (prop opinion)) " "
-          (:flag-name :key 2 :opinion (prop opinion)) " "
-          (:date-stamp :key 3 :opinion (prop opinion)) " "
-          (:author-long :key 4 :opinion (prop opinion)) " "
-          (:reply-link :key 6 :url (prop opinion url))
-          (when (prop opinion comment)
-            (:div (prop opinion comment))))))
+        (let ((opinion (prop opinions (list-last (prop tree-address))))
+              (text (if (< 1 (prop tree-address length))
+                        (or (@ opinion comment) "")
+                        (prop text))))
+          (psx
+           (:div
+            (:vote-value :key 1 :opinion opinion) " "
+            (:flag-name :key 2 :opinion opinion) " "
+            (:date-stamp :key 3 :opinion opinion) " "
+            (:author-long :key 4 :opinion opinion) " "
+            (:reply-link :key 6 :url (@ opinion url))
+            (when (has-excerpt-p opinion)
+              (if (has-found-excerpt-p opinion)
+                  (psx (:thread-excerpt
+                        :opinion opinion
+                        :text text))
+                  (psx (:thread-excerpt
+                        :opinion opinion))))
+            (when (@ opinion comment)
+              (psx (:div (@ opinion comment))))))))
 
     (defun %reformat-opinions (opins)
-      (collecting
-          (labels ((proc (tree address)
-                     (dolist (branch tree)
-                       (let ((newadd (chain address (concat (@ branch 0 id)))))
-                         (collect (list (@ branch 0) newadd))
-                         (when (< 1 (@ branch length))
-                           (proc (chain branch (slice 1)) newadd))))))
-            (proc opins (list)))))
+      (let ((opinstore (create)))
+        (list
+         (collecting
+             (labels ((proc (tree address)
+                        (dolist (branch tree)
+                          (let ((newadd (chain address (concat (@ branch 0 id)))))
+                            (setf (@ opinstore (@ branch 0 id)) (@ branch 0))
+                            (collect (list (@ branch 0) newadd))
+                            (when (< 1 (@ branch length))
+                              (proc (chain branch (slice 1)) newadd))))))
+               (proc opins (list))))
+         opinstore)))
 
     ;;For now, drop in place of target-root-inner
     (def-component target-root-thread
@@ -38,8 +53,12 @@
          (:div
           (:div "Title:" (prop title))
           (collecting
-              (dolist (op (%reformat-opinions (prop opinions)))
-                (collect
-                    (psx (:thread-opinion :opinion (@ op 0) :tree-address (@ op 1)))))))))
+              (let ((data (%reformat-opinions (prop opinions))))
+                (dolist (op (@ data 0))
+                  (collect
+                      (psx (:thread-opinion
+                            :opinions (@ data 1)
+                            :text (prop text)
+                            :tree-address op)))))))))
 
     ))
