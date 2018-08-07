4007c1fb221f0609b460557dc4f3f89984e4b0ec
bnmcgn@gmail.com <bnmcgn@gmail.com>
2018-08-07 07:07:33 -0700

    Fix path for travis

--- t/tests.lisp
+++ t/tests.lisp
@@ -1,16 +1,20 @@
 
 (in-package #:test-warflagger)
 
+;;FIXME: tmpdir should be handled differently on non-travis test runs
+(let ((tmpdir "")
+      (*cache-path* (merge-pathnames "cache/" tmpdir))
+      (*warstats-path* (merge-pathnames "warstats/" tmpdir))
+      ;;FIXME: Need better general way to handle db credentials.
+      (*test-db-connect-spec* '( :unix "test_opinions" "ben" "test_password"))
+      (*db-connect-type* :postgresql-socket3))
+  (ensure-directories-exist *cache-path*)
+  (ensure-directories-exist *warstats-path*)
+  (clsql:connect *test-db-connect-spec* *db-connect-type*)
 
-(with-temporary-directory (:pathname tmpdir)
-  (let ((*cache-path* (merge-pathnames "/cache/" tmpdir))
-        (*warstats-path* (merge-pathnames "/warstats/" tmpdir))
-        (*test-db-connect-spec* '( :unix "test_opinions" "postgres" ""))
-        (*db-connect-type* :postgresql-socket3))
-   (ensure-directories-exist *cache-path*)
-   (ensure-directories-exist *warstats-path*)
-   (clsql:connect *test-db-connect-spec* *db-connect-type*)
-
-   (test-textract)
-   (test-db)
-   ))
+  (test-textract)
+  (test-db)
+  ;;FIXME: think this through
+  ;;(delete-dir *cache-path* :recursive t)
+  ;;(delete-dir *warstats-path* :recursive t)
+  )
