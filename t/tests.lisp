d08368116e4c9f1bfd32cd707ee2a368c99579b2
bnmcgn@gmail.com <bnmcgn@gmail.com>
2018-08-07 07:21:58 -0700

    Small fix

--- t/tests.lisp
+++ t/tests.lisp
@@ -2,12 +2,12 @@
 (in-package #:test-warflagger)
 
 ;;FIXME: tmpdir should be handled differently on non-travis test runs
-(let ((tmpdir "")
-      (*cache-path* (merge-pathnames "cache/" tmpdir))
-      (*warstats-path* (merge-pathnames "warstats/" tmpdir))
-      ;;FIXME: Need better general way to handle db credentials.
-      (*test-db-connect-spec* '( :unix "test_opinions" "ben" "test_password"))
-      (*db-connect-type* :postgresql-socket3))
+(let* ((tmpdir "")
+       (*cache-path* (merge-pathnames "cache/" tmpdir))
+       (*warstats-path* (merge-pathnames "warstats/" tmpdir))
+       ;;FIXME: Need better general way to handle db credentials.
+       (*test-db-connect-spec* '( :unix "test_opinions" "ben" "test_password"))
+       (*db-connect-type* :postgresql-socket3))
   (ensure-directories-exist *cache-path*)
   (ensure-directories-exist *warstats-path*)
   (clsql:connect *test-db-connect-spec* *db-connect-type*)
