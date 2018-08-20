f3dd216731917621faee0b9f96467d60a2a38f93
bnmcgn@gmail.com <bnmcgn@gmail.com>
2018-08-20 08:16:24 -0700

    Debugging 4

--- src/text-extract/textract.lisp
+++ src/text-extract/textract.lisp
@@ -165,7 +165,9 @@
   (let ((index (get-url-index url)))
     (print "in save-page-to-cache")
     (print (cache-loc url))
-    (ensure-directories-exist (make-pathname :directory (cache-loc url)))
+    ;;(ensure-directories-exist (make-pathname :directory (cache-loc url)))
+    (ensure-directories-exist (cache-loc url))
+    (print "here")
     ;;FIXME: Need a way to not cache non-existent urls. Otherwise will get major clutter
     ;; from half-typed urls and malicious users.
     (unless (probe-file *text-extractor-script*)
