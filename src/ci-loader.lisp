ddd68eb3afd14d6bd2eba80b03f2ec61df75d028
bnmcgn@gmail.com <bnmcgn@gmail.com>
2021-10-15 10:15:55 -0700

    Register-local-projects

--- src/ci-loader.lisp
+++ src/ci-loader.lisp
@@ -1,6 +1,8 @@
 (load "/tmp/quicklisp.lisp")
 (quicklisp-quickstart:install)
 
+(ql:register-local-projects)
+
 (ql:quickload 'test-warflagger)
 (if (test-warflagger:test-warflagger)
     (uiop:quit 0)
