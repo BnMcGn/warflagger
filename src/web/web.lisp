c86d8524f916c273d5f1087e8cdfc359b6770026
bnmcgn@gmail.com <bnmcgn@gmail.com>
2021-11-12 06:12:22 -0800

    Grouped2 done

--- src/web/web.lisp
+++ src/web/web.lisp
@@ -305,11 +305,6 @@
             ()
           (grouped-page)))
 
-  (setf (ningle:route *app* "/grouped2/*")
-        (quick-page
-            ()
-          (grouped-page2)))
-
   (setf (ningle:route *app* "/faq/")
         (quick-page ()
           (html-out
