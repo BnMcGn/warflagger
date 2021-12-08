d6ac75caf5f20da5f40601cad290a2769fb0993b
bnmcgn@gmail.com <bnmcgn@gmail.com>
2021-12-08 06:04:00 -0800

    Added posting tool, opinion formatter

--- src/admin.lisp
+++ src/admin.lisp
@@ -24,6 +24,55 @@
 
 (defparameter *user* nil)
 (defparameter *target* nil)
+(defparameter *posting-method* nil)
+(defparameter *posting-directory* nil)
+(defparameter *opinion-queue* nil)
+(defparameter *local-opinion-store* nil)
+
+
+(defun post (opinion) (%post opinion *posting-method*))
+(defgeneric %post (opinion posting-method))
+(defmethod %post (opinion (posting-method (eql :directory)))
+  (if *posting-directory*
+      (warflagger::save-opinion-to-folder opinion *posting-directory*)
+      (error "Can't post to directory with *posting-directory* unset")))
+;;FIXME:: add a non-post, save in memory
+
+
+;;Opinion makers
+
+(defun prep-opinion (&key comment excerpt excerpt-offset flag reference
+                          (target *target*) (author *user*))
+  (prep-opinion-alist
+   (list*
+    (when comment (cons :comment comment))
+    (when excerpt (cons :excerpt excerpt))
+    (when excerpt-offset (cons :excerpt-offset excerpt-offset))
+    (when flag (cons :flag flag))
+    (when reference (cons :reference reference)))
+   :target target :author author))
+
+(defun prep-opinion-alist (opinion &key (target *target*) (author *user*))
+  (let ((have-author (assoc :author opinion))
+        (have-target (assoc :target opinion)))
+    (list*
+     (unless have-target
+       (if target (cons :target target) (error "No target available")))
+     (unless have-author
+       (if author (cons :author author) (error "No author available")))
+     opinion)))
+
+(defun comment (comment)
+  (post (prep-opinion :flag (list :custodial :blank) comment)))
+
+;;FIXME: always fetches original text, not updates. Also, should we do :clean-comment?
+(defun target-text (&optional target *target*)
+  "Target is specified by url or iid, generally in the *target* var."
+  (if (warflagger:iid-p target)
+      (or (and *local-opinion-store*
+               (gethash target *local-opinion-store*))
+          (error "IID target not found"))
+      (gethash :text (warflagger:text-server-dispatcher target))))
 
 
 ;;CSV input format:
