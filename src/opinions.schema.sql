07d802273ee20db90749f2cbabd7c04b5d04f310
bnmcgn@gmail.com <bnmcgn@gmail.com>
2023-06-27 10:37:33 -0700

    Schema updated
    - Added unique constraint to rooturl.rooturl

--- src/opinions.schema.sql
+++ src/opinions.schema.sql
@@ -2,14 +2,16 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 9.5.10
--- Dumped by pg_dump version 9.5.10
+-- Dumped from database version 9.5.25
+-- Dumped by pg_dump version 9.5.25
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
+SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
@@ -27,8 +29,6 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 
-SET search_path = public, pg_catalog;
-
 SET default_tablespace = '';
 
 SET default_with_oids = false;
@@ -37,20 +37,20 @@ SET default_with_oids = false;
 -- Name: author; Type: TABLE; Schema: public; Owner: ben
 --
 
-CREATE TABLE author (
+CREATE TABLE public.author (
     id integer,
     type character varying(20),
     value text
 );
 
 
-ALTER TABLE author OWNER TO ben;
+ALTER TABLE public.author OWNER TO ben;
 
 --
 -- Name: author_id_seq; Type: SEQUENCE; Schema: public; Owner: ben
 --
 
-CREATE SEQUENCE author_id_seq
+CREATE SEQUENCE public.author_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -58,25 +58,25 @@ CREATE SEQUENCE author_id_seq
     CACHE 1;
 
 
-ALTER TABLE author_id_seq OWNER TO ben;
+ALTER TABLE public.author_id_seq OWNER TO ben;
 
 --
 -- Name: comment; Type: TABLE; Schema: public; Owner: ben
 --
 
-CREATE TABLE comment (
+CREATE TABLE public.comment (
     opinion integer NOT NULL,
     comment character varying
 );
 
 
-ALTER TABLE comment OWNER TO ben;
+ALTER TABLE public.comment OWNER TO ben;
 
 --
 -- Name: comment_opinion_seq; Type: SEQUENCE; Schema: public; Owner: ben
 --
 
-CREATE SEQUENCE comment_opinion_seq
+CREATE SEQUENCE public.comment_opinion_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -84,33 +84,33 @@ CREATE SEQUENCE comment_opinion_seq
     CACHE 1;
 
 
-ALTER TABLE comment_opinion_seq OWNER TO ben;
+ALTER TABLE public.comment_opinion_seq OWNER TO ben;
 
 --
 -- Name: comment_opinion_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: ben
 --
 
-ALTER SEQUENCE comment_opinion_seq OWNED BY comment.opinion;
+ALTER SEQUENCE public.comment_opinion_seq OWNED BY public.comment.opinion;
 
 
 --
 -- Name: excerpt; Type: TABLE; Schema: public; Owner: ben
 --
 
-CREATE TABLE excerpt (
+CREATE TABLE public.excerpt (
     opinion integer,
     type character varying(20),
     value character varying
 );
 
 
-ALTER TABLE excerpt OWNER TO ben;
+ALTER TABLE public.excerpt OWNER TO ben;
 
 --
 -- Name: looks; Type: TABLE; Schema: public; Owner: ben
 --
 
-CREATE TABLE looks (
+CREATE TABLE public.looks (
     firstlook timestamp without time zone NOT NULL,
     wf_user text NOT NULL,
     rootid integer NOT NULL,
@@ -118,13 +118,13 @@ CREATE TABLE looks (
 );
 
 
-ALTER TABLE looks OWNER TO ben;
+ALTER TABLE public.looks OWNER TO ben;
 
 --
 -- Name: opinion; Type: TABLE; Schema: public; Owner: ben
 --
 
-CREATE TABLE opinion (
+CREATE TABLE public.opinion (
     id integer NOT NULL,
     author integer NOT NULL,
     flag character varying(50) NOT NULL,
@@ -137,13 +137,13 @@ CREATE TABLE opinion (
 );
 
 
-ALTER TABLE opinion OWNER TO ben;
+ALTER TABLE public.opinion OWNER TO ben;
 
 --
 -- Name: opinion_id_seq; Type: SEQUENCE; Schema: public; Owner: ben
 --
 
-CREATE SEQUENCE opinion_id_seq
+CREATE SEQUENCE public.opinion_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -151,32 +151,32 @@ CREATE SEQUENCE opinion_id_seq
     CACHE 1;
 
 
-ALTER TABLE opinion_id_seq OWNER TO ben;
+ALTER TABLE public.opinion_id_seq OWNER TO ben;
 
 --
 -- Name: opinion_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: ben
 --
 
-ALTER SEQUENCE opinion_id_seq OWNED BY opinion.id;
+ALTER SEQUENCE public.opinion_id_seq OWNED BY public.opinion.id;
 
 
 --
 -- Name: reference; Type: TABLE; Schema: public; Owner: ben
 --
 
-CREATE TABLE reference (
+CREATE TABLE public.reference (
     opinion integer NOT NULL,
     reference text
 );
 
 
-ALTER TABLE reference OWNER TO ben;
+ALTER TABLE public.reference OWNER TO ben;
 
 --
 -- Name: reference_opinion_seq; Type: SEQUENCE; Schema: public; Owner: ben
 --
 
-CREATE SEQUENCE reference_opinion_seq
+CREATE SEQUENCE public.reference_opinion_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -184,33 +184,33 @@ CREATE SEQUENCE reference_opinion_seq
     CACHE 1;
 
 
-ALTER TABLE reference_opinion_seq OWNER TO ben;
+ALTER TABLE public.reference_opinion_seq OWNER TO ben;
 
 --
 -- Name: reference_opinion_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: ben
 --
 
-ALTER SEQUENCE reference_opinion_seq OWNED BY reference.opinion;
+ALTER SEQUENCE public.reference_opinion_seq OWNED BY public.reference.opinion;
 
 
 --
 -- Name: rooturl; Type: TABLE; Schema: public; Owner: ben
 --
 
-CREATE TABLE rooturl (
+CREATE TABLE public.rooturl (
     id integer NOT NULL,
     rooturl text,
     rooturl_real boolean NOT NULL
 );
 
 
-ALTER TABLE rooturl OWNER TO ben;
+ALTER TABLE public.rooturl OWNER TO ben;
 
 --
 -- Name: rooturl_id_seq; Type: SEQUENCE; Schema: public; Owner: ben
 --
 
-CREATE SEQUENCE rooturl_id_seq
+CREATE SEQUENCE public.rooturl_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
@@ -218,48 +218,48 @@ CREATE SEQUENCE rooturl_id_seq
     CACHE 1;
 
 
-ALTER TABLE rooturl_id_seq OWNER TO ben;
+ALTER TABLE public.rooturl_id_seq OWNER TO ben;
 
 --
 -- Name: rooturl_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: ben
 --
 
-ALTER SEQUENCE rooturl_id_seq OWNED BY rooturl.id;
+ALTER SEQUENCE public.rooturl_id_seq OWNED BY public.rooturl.id;
 
 
 --
 -- Name: opinion; Type: DEFAULT; Schema: public; Owner: ben
 --
 
-ALTER TABLE ONLY comment ALTER COLUMN opinion SET DEFAULT nextval('comment_opinion_seq'::regclass);
+ALTER TABLE ONLY public.comment ALTER COLUMN opinion SET DEFAULT nextval('public.comment_opinion_seq'::regclass);
 
 
 --
 -- Name: id; Type: DEFAULT; Schema: public; Owner: ben
 --
 
-ALTER TABLE ONLY opinion ALTER COLUMN id SET DEFAULT nextval('opinion_id_seq'::regclass);
+ALTER TABLE ONLY public.opinion ALTER COLUMN id SET DEFAULT nextval('public.opinion_id_seq'::regclass);
 
 
 --
 -- Name: opinion; Type: DEFAULT; Schema: public; Owner: ben
 --
 
-ALTER TABLE ONLY reference ALTER COLUMN opinion SET DEFAULT nextval('reference_opinion_seq'::regclass);
+ALTER TABLE ONLY public.reference ALTER COLUMN opinion SET DEFAULT nextval('public.reference_opinion_seq'::regclass);
 
 
 --
 -- Name: id; Type: DEFAULT; Schema: public; Owner: ben
 --
 
-ALTER TABLE ONLY rooturl ALTER COLUMN id SET DEFAULT nextval('rooturl_id_seq'::regclass);
+ALTER TABLE ONLY public.rooturl ALTER COLUMN id SET DEFAULT nextval('public.rooturl_id_seq'::regclass);
 
 
 --
 -- Name: comment_pkey; Type: CONSTRAINT; Schema: public; Owner: ben
 --
 
-ALTER TABLE ONLY comment
+ALTER TABLE ONLY public.comment
     ADD CONSTRAINT comment_pkey PRIMARY KEY (opinion);
 
 
@@ -267,7 +267,7 @@ ALTER TABLE ONLY comment
 -- Name: opinion_pkey; Type: CONSTRAINT; Schema: public; Owner: ben
 --
 
-ALTER TABLE ONLY opinion
+ALTER TABLE ONLY public.opinion
     ADD CONSTRAINT opinion_pkey PRIMARY KEY (id);
 
 
@@ -275,7 +275,7 @@ ALTER TABLE ONLY opinion
 -- Name: reference_pkey; Type: CONSTRAINT; Schema: public; Owner: ben
 --
 
-ALTER TABLE ONLY reference
+ALTER TABLE ONLY public.reference
     ADD CONSTRAINT reference_pkey PRIMARY KEY (opinion);
 
 
@@ -283,18 +283,164 @@ ALTER TABLE ONLY reference
 -- Name: rooturl_pkey; Type: CONSTRAINT; Schema: public; Owner: ben
 --
 
-ALTER TABLE ONLY rooturl
+ALTER TABLE ONLY public.rooturl
     ADD CONSTRAINT rooturl_pkey PRIMARY KEY (id);
 
 
 --
--- Name: public; Type: ACL; Schema: -; Owner: postgres
+-- Name: rooturl_unique_constraint; Type: CONSTRAINT; Schema: public; Owner: ben
+--
+
+ALTER TABLE ONLY public.rooturl
+    ADD CONSTRAINT rooturl_unique_constraint UNIQUE (rooturl);
+
+
+--
+-- Name: SCHEMA public; Type: ACL; Schema: -; Owner: postgres
 --
 
 REVOKE ALL ON SCHEMA public FROM PUBLIC;
 REVOKE ALL ON SCHEMA public FROM postgres;
 GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+--
+-- Name: TABLE author; Type: ACL; Schema: public; Owner: ben
+--
+
+REVOKE ALL ON TABLE public.author FROM PUBLIC;
+REVOKE ALL ON TABLE public.author FROM ben;
+GRANT ALL ON TABLE public.author TO ben;
+GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.author TO warflagger;
+
+
+--
+-- Name: SEQUENCE author_id_seq; Type: ACL; Schema: public; Owner: ben
+--
+
+REVOKE ALL ON SEQUENCE public.author_id_seq FROM PUBLIC;
+REVOKE ALL ON SEQUENCE public.author_id_seq FROM ben;
+GRANT ALL ON SEQUENCE public.author_id_seq TO ben;
+GRANT SELECT,UPDATE ON SEQUENCE public.author_id_seq TO warflagger;
+
+
+--
+-- Name: TABLE comment; Type: ACL; Schema: public; Owner: ben
+--
+
+REVOKE ALL ON TABLE public.comment FROM PUBLIC;
+REVOKE ALL ON TABLE public.comment FROM ben;
+GRANT ALL ON TABLE public.comment TO ben;
+GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.comment TO warflagger;
+
+
+--
+-- Name: SEQUENCE comment_opinion_seq; Type: ACL; Schema: public; Owner: ben
+--
+
+REVOKE ALL ON SEQUENCE public.comment_opinion_seq FROM PUBLIC;
+REVOKE ALL ON SEQUENCE public.comment_opinion_seq FROM ben;
+GRANT ALL ON SEQUENCE public.comment_opinion_seq TO ben;
+GRANT SELECT,UPDATE ON SEQUENCE public.comment_opinion_seq TO warflagger;
+
+
+--
+-- Name: TABLE excerpt; Type: ACL; Schema: public; Owner: ben
+--
+
+REVOKE ALL ON TABLE public.excerpt FROM PUBLIC;
+REVOKE ALL ON TABLE public.excerpt FROM ben;
+GRANT ALL ON TABLE public.excerpt TO ben;
+GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.excerpt TO warflagger;
+
+
+--
+-- Name: TABLE looks; Type: ACL; Schema: public; Owner: ben
+--
+
+REVOKE ALL ON TABLE public.looks FROM PUBLIC;
+REVOKE ALL ON TABLE public.looks FROM ben;
+GRANT ALL ON TABLE public.looks TO ben;
+GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.looks TO warflagger;
+
+
+--
+-- Name: TABLE opinion; Type: ACL; Schema: public; Owner: ben
+--
+
+REVOKE ALL ON TABLE public.opinion FROM PUBLIC;
+REVOKE ALL ON TABLE public.opinion FROM ben;
+GRANT ALL ON TABLE public.opinion TO ben;
+GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.opinion TO warflagger;
+
+
+--
+-- Name: SEQUENCE opinion_id_seq; Type: ACL; Schema: public; Owner: ben
+--
+
+REVOKE ALL ON SEQUENCE public.opinion_id_seq FROM PUBLIC;
+REVOKE ALL ON SEQUENCE public.opinion_id_seq FROM ben;
+GRANT ALL ON SEQUENCE public.opinion_id_seq TO ben;
+GRANT SELECT,UPDATE ON SEQUENCE public.opinion_id_seq TO warflagger;
+
+
+--
+-- Name: TABLE reference; Type: ACL; Schema: public; Owner: ben
+--
+
+REVOKE ALL ON TABLE public.reference FROM PUBLIC;
+REVOKE ALL ON TABLE public.reference FROM ben;
+GRANT ALL ON TABLE public.reference TO ben;
+GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.reference TO warflagger;
+
+
+--
+-- Name: SEQUENCE reference_opinion_seq; Type: ACL; Schema: public; Owner: ben
+--
+
+REVOKE ALL ON SEQUENCE public.reference_opinion_seq FROM PUBLIC;
+REVOKE ALL ON SEQUENCE public.reference_opinion_seq FROM ben;
+GRANT ALL ON SEQUENCE public.reference_opinion_seq TO ben;
+GRANT SELECT,UPDATE ON SEQUENCE public.reference_opinion_seq TO warflagger;
+
+
+--
+-- Name: TABLE rooturl; Type: ACL; Schema: public; Owner: ben
+--
+
+REVOKE ALL ON TABLE public.rooturl FROM PUBLIC;
+REVOKE ALL ON TABLE public.rooturl FROM ben;
+GRANT ALL ON TABLE public.rooturl TO ben;
+GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.rooturl TO warflagger;
+
+
+--
+-- Name: SEQUENCE rooturl_id_seq; Type: ACL; Schema: public; Owner: ben
+--
+
+REVOKE ALL ON SEQUENCE public.rooturl_id_seq FROM PUBLIC;
+REVOKE ALL ON SEQUENCE public.rooturl_id_seq FROM ben;
+GRANT ALL ON SEQUENCE public.rooturl_id_seq TO ben;
+GRANT SELECT,UPDATE ON SEQUENCE public.rooturl_id_seq TO warflagger;
+
+
+--
+-- Name: DEFAULT PRIVILEGES FOR SEQUENCES; Type: DEFAULT ACL; Schema: public; Owner: postgres
+--
+
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public REVOKE ALL ON SEQUENCES  FROM PUBLIC;
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public REVOKE ALL ON SEQUENCES  FROM postgres;
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT SELECT,UPDATE ON SEQUENCES  TO warflagger;
+
+
+--
+-- Name: DEFAULT PRIVILEGES FOR TABLES; Type: DEFAULT ACL; Schema: public; Owner: postgres
+--
+
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public REVOKE ALL ON TABLES  FROM PUBLIC;
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public REVOKE ALL ON TABLES  FROM postgres;
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT SELECT,INSERT,DELETE,UPDATE ON TABLES  TO warflagger;
 
 
 --
