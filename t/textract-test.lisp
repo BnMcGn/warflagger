1644983da885afdd6bfb43c0b1a17c3511118d30
bnmcgn@gmail.com <bnmcgn@gmail.com>
2024-09-16 09:55:37 -0700

    Bugfixes

--- t/textract-test.lisp
+++ t/textract-test.lisp
@@ -15,7 +15,7 @@
   nil)
 
 (test core-extractor "Test the extraction process"
-  (multiple-value-bind (title text metadata links) (warflagger::tt-extract *sample-file*)
+  (multiple-value-bind (title text metadata links) (warflagger::tt-extract-html *sample-file*)
     (is (string= title "Sample Web Page"))
     (is (stringp text))
     (is (sequence-starts-with text "Sample"))
@@ -33,48 +33,16 @@
 (test bad-page "Handle bad page"
   (let ((test-url2 (strcat *testurl* "x")))
     (unless (wf/ipfs:extraction-attempted? test-url2)
-      (tt-update-page-data test-url2))
+      (tt-update-page-data test-url2 "text/html"))
     (is (not (wf/ipfs:extracted? test-url2)))
     (is (wf/ipfs:extraction-attempted? test-url2))
     (is (getf (wf/ipfs:ipfs-extracted-metadata test-url2) :errors))))
 
 (test extracted-page "Test extracted page storage"
   (unless (wf/ipfs:extracted? *testurl*)
-    (tt-update-page-data *testurl* *sample-file*))
+    (tt-update-page-data *testurl* *sample-file* "text/html"))
   (is (wf/ipfs:extraction-attempted? *testurl*))
   (is (wf/ipfs:extracted? *testurl*))
   (is (string= (wf/ipfs:ipfs-extracted-title *testurl*) "Sample Web Page"))
   (is (stringp (wf/ipfs:ipfs-extracted-text *testurl*)))
   (is (sequence-starts-with (wf/ipfs:ipfs-extracted-text *testurl*) "Sample")))
-
-#|
-(test sanity-checks "Basic Sanity Checks"
-  ;; Note: these assume a fresh (empty) cache directory
-  (is (null (is-cached *testurl*)))
-  (is (null (is-fresh *testurl*)))
-  (is (null (has-failure *testurl*)))
-  (is (null (fresh-failure *testurl*)))
-  (is (null (is-pending *testurl*)))
-  (is (null (old-page-available *testurl*)))
-  (is (= 0 (length (hash-table-keys *byurl*)))))
-
-(test process-page "Process a page"
-  (update-page *testurl*)
-  (try-awhile (lambda () (old-page-available *testurl*)) :sleep 0.5 :wait 8.0)
-  (print "Printing script errors")
-  (print (grab-messages *testurl*))
-  (is (is-cached *testurl*))
-  (is (string= (grab-title *testurl*) "Sample Web Page"))
-  (is (stringp (grab-text *testurl*)))
-  (is (sequence-starts-with (grab-text *testurl*) "Sample")))
-
-(test bad-page "Handle bad page"
-  (let ((test-url2 (strcat *testurl* "x")))
-    (update-page test-url2)
-    (sleep 2.0)
-    (is (is-cached test-url2))
-    (is (not (null (has-failure test-url2))))
-    (is (not (null (fresh-failure test-url2))))
-    (is (null (is-pending test-url2)))))
-
-|#
